4be4632451fdfd8cc67e8ec47562b85381b07227
leoregino <lrreginoa@gmail.com>
2020-10-20 00:29:44 +0200

    Working dashboard with neat code

--- Preprocessing.R
+++ Preprocessing.R
@@ -6,161 +6,74 @@ library(tidyverse)
 library(ggplot2)
 library(EpiEstim)
 
-
+### 1. Load from web ### 
 url <- "https://www.datos.gov.co/resource/gt2j-8ykr.json?$limit="
-nb_limit <- "10000"
+nb_limit <- "1000000"
 datosImport <- fromJSON(paste0(url,nb_limit))
 
+### 2. Columns containing NA in JSON ###
 total_row <- nrow(datosImport)
 
 col_NA <- data.frame( matrix(ncol = 2, nrow = 0) )
-colnames(col_NA) <- c("Variable", "Nb_NA") 
+colnames(col_NA) <- c("Variable", "Perc_NA") 
 
-
-#col_to_treat <- c() # Columns having NA, to be processed
+### 2.1 Save proportion of NA in columns ###
 for (nom_col in colnames(datosImport)){
     nro_NA <- nrow(datosImport[is.na.data.frame(datosImport[nom_col]), ]) 
     if (nro_NA>0){
-      #col_to_treat <- c(col_to_treat,nom_col) # Append column's name to process 
-      col_NA[nrow(col_NA) + 1,] = c(nom_col, nro_NA/total_row*100 )
+      col_NA[nrow(col_NA) + 1,] = c(nom_col, round(nro_NA/total_row*100,1)  )
     }
-    #print(paste0(nom_col, str(nro_NA)))
 }
 col_NA$Nb_NA <- as.numeric(col_NA$Nb_NA) 
 
-# Plot % of missing values in columns
-p <- ggplot(data = col_NA, aes(x = Variable, y = Nb_NA) ) +
+### 2.2 Plot Percentage of missing values in columns ###
+q <- ggplot(data = col_NA, aes(x = Variable, y = Perc_NA) ) +
   geom_bar(stat="identity", fill = "blue") +
-  geom_text(aes(label=Nb_NA), vjust = 1.6, color = "black", size = 3) +
-  labs(title = "Number of NA per variable")
+  geom_text(aes(label=Perc_NA), vjust = 1.6, color = "black", size = 3) +
+  labs(title = "Percentage of NA per variable")
 
-p
+q
 
-############ TREAT THE MISSING VALUES ############
+### 3. TREAT THE MISSING VALUES ###
 
-# 1. [pa_s_de_procedencia]: If NA --> Assign = COLOMBIA
+# 3.1. [pa_s_de_procedencia]: If NA --> Assign = COLOMBIA
 datosImport[is.na.data.frame(datosImport$pa_s_de_procedencia), ]$pa_s_de_procedencia <- "COLOMBIA"
 
-# 2. [fis]: If NA --> Assign = fecha_diagnostico - 10
+# 3.2. [fecha_diagnostico]: If NA --> Assign = 0001-01-01T00:00:00.000
+datosImport[is.na.data.frame(datosImport$fecha_diagnostico), ]$fecha_diagnostico <- datosImport[is.na.data.frame(datosImport$fecha_diagnostico), ]$fecha_reporte_web
+
+# 3.3. [fis]: If NA --> Assign = fecha_diagnostico - 10
 datosImport[is.na.data.frame(datosImport$fis), ]$fis <- datosImport[is.na.data.frame(datosImport$fis), ]$fecha_diagnostico
 
-# 3. [fecha_diagnostico]: If NA --> Assign = 0001-01-01T00:00:00.000
-datosImport[is.na.data.frame(datosImport$fecha_diagnostico), ]$fecha_diagnostico <-  "0001-01-01T00:00:00.000"
-
-# 4. [fecha_recuperado]: IF NA --> 0001-01-01T00:00:00.000
+# 3.4. [fecha_recuperado]: IF NA --> 0001-01-01T00:00:00.000
 datosImport[is.na.data.frame(datosImport$fecha_recuperado), ]$fecha_recuperado <-  "0001-01-01T00:00:00.000"
 
-# 5. [tipo_recuperaci_n]: If NA (Vivo o Fallecido) --> Assign = "NA" 
+# 3.5. [tipo_recuperaci_n]: If NA (Vivo o Fallecido) --> Assign = "NA" 
 datosImport[is.na.data.frame(datosImport$tipo_recuperaci_n), ]$tipo_recuperaci_n <-  "NA" 
 
-# 6. [codigo_pais]: If NA (COLOMBIA) --> Assign = 170 (code ISO 3166-1)
+# 3.6. [codigo_pais]: If NA (COLOMBIA) --> Assign = 170 (code ISO 3166-1)
 datosImport[is.na.data.frame(datosImport$codigo_pais), ]$codigo_pais <-  170 
 
-# 7. [nombre_grupo_etnico]: IF NA <-- Assign = "Otro"
+# 3.7. [nombre_grupo_etnico]: IF NA <-- Assign = "Otro"
 datosImport[is.na.data.frame(datosImport$nombre_grupo_etnico), ]$nombre_grupo_etnico <-  "Otro" 
 
-# 8. [fecha_de_muerte]: IF NA (vivo o Recuperado ) <-- Assign = 0001-01-01T00:00:00.000 
+# 3.8. [fecha_de_muerte]: IF NA (vivo o Recuperado ) <-- Assign = 0001-01-01T00:00:00.000 
 datosImport[is.na.data.frame(datosImport$fecha_de_muerte), ]$fecha_de_muerte <-  "0001-01-01T00:00:00.000" 
 
+# 3.9. [pertenencia_etnica]: IG NA --> Assign = "Otro"
+datosImport[is.na.data.frame(datosImport$pertenencia_etnica), ]$pertenencia_etnica <-  "Otro" 
+
+
+### 4. WRITE CSV ###
+file_name <- "C:/Users/Leonardo REGINO/Documents/DATAMINH/Covid19/Dashboards/Data/Data_Covid19_Colombia.csv"
+write.csv(datosImport, file_name, row.names = FALSE  )
+
+### 5. Test file by reading ###
+myDataCovid <- read.csv(file = file_name, sep = ",", header = TRUE  )
 
 
 
 
 
-a <- data[26:50,] 
-
-for (i in 1:nrow(a) ){
-  print( is.na.data.frame(a[i,"fis"]) )
-}
 
 
-#############3 
-library(EpiEstim)
-
-data(Flu2009)
-datosImport$nb <-1 
-
-mis_datos <- datosImport %>% 
-              filter(departamento == "Bogotá D.C." ) %>%
-              filter(ciudad_de_ubicaci_n == "Bogotá D.C." ) %>%
-              group_by(fecha_de_notificaci_n) %>% 
-              summarise_if(is.numeric, sum, na.rm=TRUE)
-
-mis_datos <- as.data.frame(mis_datos) 
-mis_datos$fecha_de_notificaci_n <- as.Date(mis_datos$fecha_de_notificaci_n)
-
-mis_datos <- mis_datos[seq(4, nrow(mis_datos)) , ]
-
-
-mis_datos <- mis_datos %>% 
-  rename(
-    dates = fecha_de_notificaci_n,
-    I = nb
-  )
-
-
-res_parametric_si <- estimate_R(mis_datos, 
-                               method="parametric_si",
-                               config = make_config(list(
-                                 mean_si = 2.6, 
-                                 std_si = 1.5))
-)
-
-
-  
-rest <- estimate_R(
-  mis_datos,
-  method = "uncertain_si",
-  config = make_config(
-    list(
-      mean_si = 2.6, 
-      std_mean_si = 1,
-      min_mean_si = 1, 
-      max_mean_si = 4.2,
-      std_si = 1.5, 
-      std_std_si = 0.5,
-      min_std_si = 0.5, 
-      max_std_si = 2.5,
-      n1 = 100, n2 = 100
-    )
-  )
-)
-
-plot(rest)
-
-
-
-
-################################
-
-rest <- estimate_R(
-  d$local,
-  method = "uncertain_si",
-  config = make_config(
-    list(
-      mean_si = 2.6, 
-      std_mean_si = 1,
-      min_mean_si = 1, 
-      max_mean_si = 4.2,
-      std_si = 1.5, 
-      std_std_si = 0.5,
-      min_std_si = 0.5, 
-      max_std_si = 2.5,
-      n1 = 100, n2 = 100
-    )
-  )
-)
-
-death_df <- datosImport %>%
-  filter(atenci_n == "Fallecido") %>%
-  group_by(fecha_de_notificaci_n) %>%
-  summarise_if(is.numeric, sum, na.rm = TRUE)
-d$type <- "Contaminado"
-death_df$type <- "Fallecido"
-################################
-
-
-
-## Deploy 
-library(rsconnect)
-rsconnect::deployApp('C:/Users/Leonardo REGINO/Documents/DATAMINH/Covid19/Dashboards')
--- app.R
+++ app.R
@@ -16,47 +16,17 @@ library(ggplot2)
 library(EpiEstim)
 
 ### DIRECTORY PARAMETERS ###
-#file_path <- "C:/Users/Leonardo REGINO/Documents/DATAMINH/Covid19/Dashboards/Data"
-#file_name <- "Covid19_Colombia.json"
-#datosImport <- fromJSON(txt = paste(file_path, file_name, sep = "/") )
 
-url <- "https://www.datos.gov.co/resource/gt2j-8ykr.json?$limit="
-nb_limit <- "10000"
-datosImport <- fromJSON(paste0(url,nb_limit))
+# Load from local directory
+### 1. Load file from local ###
+file_name <- "C:/Users/Leonardo REGINO/Documents/DATAMINH/Covid19/Dashboards/Data/Data_Covid19_Colombia.csv"
+DataCovid19 <-  read.csv(file = file_name, sep = ",", header = TRUE )
 
 
-############ TREAT THE MISSING VALUES ############
+### 4. Aggregate data ###
+DataCovid19 <- cbind(DataCovid19, data.frame(nb= rep(1,nrow(DataCovid19)) ) )
 
-# 1. [pa_s_de_procedencia]: If NA --> Assign = COLOMBIA
-datosImport[is.na.data.frame(datosImport$pa_s_de_procedencia), ]$pa_s_de_procedencia <- "COLOMBIA"
-
-# 2. [fis]: If NA --> Assign = fecha_diagnostico - 10
-datosImport[is.na.data.frame(datosImport$fis), ]$fis <- datosImport[is.na.data.frame(datosImport$fis), ]$fecha_diagnostico
-
-# 3. [fecha_diagnostico]: If NA --> Assign = 0001-01-01T00:00:00.000
-datosImport[is.na.data.frame(datosImport$fecha_diagnostico), ]$fecha_diagnostico <-  "0001-01-01T00:00:00.000"
-
-# 4. [fecha_recuperado]: IF NA --> 0001-01-01T00:00:00.000
-datosImport[is.na.data.frame(datosImport$fecha_recuperado), ]$fecha_recuperado <-  "0001-01-01T00:00:00.000"
-
-# 5. [tipo_recuperaci_n]: If NA (Vivo o Fallecido) --> Assign = "NA" 
-datosImport[is.na.data.frame(datosImport$tipo_recuperaci_n), ]$tipo_recuperaci_n <-  "NA" 
-
-# 6. [codigo_pais]: If NA (COLOMBIA) --> Assign = 170 (code ISO 3166-1)
-datosImport[is.na.data.frame(datosImport$codigo_pais), ]$codigo_pais <-  170 
-
-# 7. [nombre_grupo_etnico]: IF NA <-- Assign = "Otro"
-datosImport[is.na.data.frame(datosImport$nombre_grupo_etnico), ]$nombre_grupo_etnico <-  "Otro" 
-
-# 8. [fecha_de_muerte]: IF NA (vivo o Recuperado ) <-- Assign = 0001-01-01T00:00:00.000 
-datosImport[is.na.data.frame(datosImport$fecha_de_muerte), ]$fecha_de_muerte <-  "0001-01-01T00:00:00.000" 
-
-# datosImport[is.na.data.frame(datosImport$fis), ]$fis <- datosImport[is.na.data.frame(datosImport$fis), ]$fecha_diagnostico
-# datosImport[is.na.data.frame(datosImport$pa_s_de_procedencia), ]$pa_s_de_procedencia <- "COLOMBIA"
-
-#datosImport$nb <- 1 # To be able to count/aggregate
-
-#datosImport$fis <- as.Date(datosImport$fis)
+DataCovid19$fis <- as.character.Date(DataCovid19$fis)
 
 ui <- fluidPage(
   titlePanel("Evolution of R0 - Covid19"),
@@ -77,72 +47,57 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   
-  data <- reactive({
-
-    
-    if (!is.null(input$Depto) & !is.null(input$City) ) {
-      
-      d = datosImport %>%
-        filter(departamento == input$Depto) %>% 
-        filter(ciudad_de_ubicaci_n == input$City) %>% 
-        filter(fis > input$NotifDate[1] & fis < input$NotifDate[2] )
-    }
-    
-    else{
-      d = datosImport
-    }
-    # d = d %>% 
-    #   group_by(fis) %>% 
-    #   summarise_if(is.numeric, sum, na.rm=TRUE)
-    
-    d[,"nb"] <- NA
-    d[,"nb"] <- 1
-    d
-  })
-  
   output$Dept_Filter <- renderUI({
-    
-    selectizeInput('Depto', 'Select a Department', choices = c("Choose a Department" = "", levels(factor(datosImport$departamento) ) ) )
-    
+    selectizeInput('Depto', 'Select a Department', choices = c("Choose a Department" = "", levels(factor(DataCovid19$departamento) ) ) )
   })
   
   output$City_Filter <- renderUI({
-  
-    selectizeInput('City', 'Select a City', choices = c("Choose a city" = "", levels(factor(datosImport[ datosImport$departamento ==  input$Depto, 'ciudad_de_ubicaci_n'] ) ) ) )    
-    
+    selectizeInput('City', 'Select a City', choices = c("Choose a city" = "", levels(factor(DataCovid19[ DataCovid19$departamento ==  input$Depto, 'ciudad_de_ubicaci_n'] ) ) ) )    
   })
   
   output$Date_Range <- renderUI({
-    
     dateRangeInput('NotifDate',
                   label = 'Filter by Notification Date:',
-                  start =  min(as.Date(datosImport$fis)) , end = max(as.Date(datosImport$fis))
-    )
-    
+                  start =  min(as.character.Date(DataCovid19$fis)) , end = max(as.character.Date(DataCovid19$fis))
+                  )
   })
+  
+  #Filter data
+  data_filt <- reactive({
+    
+    if (!is.null(input$Depto) & !is.null(input$City) ) {
+      
+      d = DataCovid19 %>%
+        filter(departamento == input$Depto) %>% 
+        filter(ciudad_de_ubicaci_n == input$City) %>% 
+        filter(fis >= input$NotifDate[1]) %>% 
+        filter(fis <= input$NotifDate[2] )
+    }
+    
+    else{
+      d = DataCovid19
+    }
+    
+    d
+  })
+  
+  ## Aggregate data
+  data_aggr <- reactive({
+    data <- data_filt()
+    data <- data %>% rename(dates = fis, I = nb)
+    data <- data %>% group_by(dates) %>% summarise(I = sum(I))
+    data <- data[seq(5, nrow(data)),] # The first dates contain holes, which generates errors while computeing Rt
+    data <- as.data.frame(data)
+    data$dates <- as.Date.character(data$dates)
+    data
+  })
+  
+  
   
   # plot time series
   output$ts_plot <- renderPlot({
     
-    data <- data() 
-    data$fis <- as.Date(data$fis)
-    
-    
-    data <- data %>% 
-      group_by(fis) %>% 
-      summarise(nb = sum(nb))
-    
-    
-    data <- data[seq(4, nrow(data)),]
-    data <- as.data.frame(data)
-    
-    
-    data <- data %>%
-      rename(
-        dates = fis,
-        I = nb
-      )
-    
+    data <- data_aggr() 
     
     rest <- estimate_R(
       data,
@@ -165,12 +120,12 @@ server <- function(input, output) {
     plot(rest)
     
   })
-  
-  # output$test_data <- renderTable({
-  #   data <- data()
-  #   
-  #   data
-  # })
+
+  # Render Table to test data
+   output$test_data <- renderTable({
+     #data_filt()
+     data_aggr()
+   })
 
 }
 
